5d6188d66e0f733ed18914620a3ddd96c6501626
Thanesh1177 <ntthanesh@gmail.com>
2026-05-24 01:09:55 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11', 'iOS >= 12', 'Android >= 6'],
-    }),
-  ],
+  plugins: [react()],
 
   build: {
     // Let Vite use default optimal targets for production bundling
